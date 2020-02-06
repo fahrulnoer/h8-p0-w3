c45a8036937c1d150fb5907f57b21a0eb638982d
fahrulnoer <fahrulnoer17@gmail.com>
2020-02-07 05:12:51 +0700

    update exercise-3

--- exercise-3.js
+++ exercise-3.js
@@ -1,15 +1,43 @@
 // Nama : Fahrul Muhammad Neor
 // exercise 3 week 3 phase 0
 
-var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
-dataHandling2(input);
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+];
 
-/**
- * keluaran yang diharapkan (pada console)
- *
- * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
- * Mei
- * ["1989", "21", "05"]
- * 21-05-1989
- * Roman Alamsyah
- */
+function dataHandling (input) {
+	for (var i = 0; i < input.length; i++) {
+		var result = `
+		ID : ${input[i][0]}
+		NAME : ${input[i][1]}
+		KOTA : ${input[i][2]}
+		TTL : ${input[i][3]}
+		Hobby : ${input[i][4]}`
+	console.log(result)
+	}
+}
+
+dataHandling(input)
+
+// Nomor ID:  0001
+// Nama Lengkap:  Roman Alamsyah
+// TTL:  Bandar Lampung 21/05/1989
+// Hobi:  Membaca
+
+// Nomor ID:  0002
+// Nama Lengkap:  Dika Sembiring
+// TTL:  Medan 10/10/1992
+// Hobi:  Bermain Gitar
+
+// Nomor ID:  0003
+// Nama Lengkap:  Winona
+// TTL:  Ambon 25/12/1965
+// Hobi:  Memasak
+
+// Nomor ID:  0004
+// Nama Lengkap:  Bintang Senjaya
+// TTL:  Martapura 6/4/1970
+// Hobi:  Berkebun
